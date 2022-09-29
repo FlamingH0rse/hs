25cf9c51068bca9594377ba6a5598939a689b36f
99073459+FlamingH0rse@users.noreply.github.com <99073459+FlamingH0rse@users.noreply.github.com>
2022-09-29 12:50:33 +0530

    blocktypes

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let str = "$ hi $/\n$ bye $"
 let strs = str.split('/')
-
+const validBlockTypes = ['create', 'loop']
 let codeblocks = []
 for (let line in strs) {
-    codeblocks[line] = strs[line].substring(
+    let blocktxt = strs[line].substring(
         strs[line].indexOf("$") + 1, 
         strs[line].lastIndexOf("$")
     ).trim()
+    
+    let blocktype = blocktxt.substring(
+        blocktxt.indexOf("[t->") + 1,
+        blocktxt.indexOf("]")
+    ).trim()
+    
+    if (!validBlockTypes.includes(blocktype)) return //console.error
 }
 console.log(codeblocks)
