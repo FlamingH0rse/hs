e5c53ab3cfd72e3b9260177660a5470697aea313
99073459+FlamingH0rse@users.noreply.github.com <99073459+FlamingH0rse@users.noreply.github.com>
2022-09-29 19:24:23 +0530

    variable creation done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let str = "$[createvar] h]//i $/\n$[createvar] bye $"
+let str = "$[createvar] name : flaming < \"\"$/\n$[createvar] age : bozo < \"\" $"
 let strs = str.split('$/')
 
 strs.forEach((s, i) => strs[i] = i==strs.length-1?strs[i]:strs[i] + "$")
@@ -19,9 +19,55 @@ for (let line in strs) {
     
     if (!validBlockTypes.includes(blocktype)) return console.error('noob')//console.error
     let code = {
-      str: blocktxt.replace(/\[.*?\]/, '').trim(),
-      type: blocktype
+        str: blocktxt.replace(/\[.*?\]/, '').trim() + ';',
+        type: blocktype
     }
     codeblocks.push(code)
 }
 console.log(codeblocks)
+
+//prolly will go in another file
+let memory = {}
+
+for (let block in codeblocks){
+    let blocktxt = codeblocks[block].str
+    if (codeblocks[block].type = 'createvar') {
+        const validDataTypesSym  = ['\"\"', '09', '01', '[]', '{}', '(=>)']
+        const validDataTypesStr = ['string', 'number', 'boolean', 'array', 'object', 'function']
+        
+        
+        const validDataTypes = new Map()
+        validDataTypesSym.forEach((sym, i) => validDataTypes.set(sym, validDataTypesStr[i]))
+        
+        let datatypeSym = blocktxt.substring(
+            blocktxt.lastIndexOf("<") + 1,
+            blocktxt.lastIndexOf(";")
+        ).trim()
+        if (!validDataTypesSym.includes(datatypeSym)) return console.error('wrong datatype lol')
+        let decl = blocktxt.replace(/\<.*\;/, '').trim()
+        
+        let varName = decl.split(':')[0].trim()
+        let value = decl.split(':')[1].trim()
+        
+        let datatype = validDataTypes.get(datatypeSym)
+        
+        if (datatype == 'string') value = value.toString()
+        if (datatype == 'number') value = Number(value)
+        if (datatype == 'boolean') {
+            if (value == '0' || value == 'false') value = false
+            if (value == '1' || value == 'true') value = true
+            else console.error('datatype error')
+        }
+        if (datatype == 'array') value = value.split(',')
+        if (datatype == 'object') {
+            let obj = {}
+            value.split(',').forEach(e => {
+                if(e.includes('-')) {
+                    let key = e.split('-')[0]
+                    let value = e.split('-')[1]
+                    obj[key] = value
+                }
+            })
+        }
+    }
+}
