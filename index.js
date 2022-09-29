64f3540907d32d2525a3c34546c3f2df8575eaad
99073459+FlamingH0rse@users.noreply.github.com <99073459+FlamingH0rse@users.noreply.github.com>
2022-09-29 17:22:53 +0530

    compiler(arranging done)

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-let str = "$ hi $/\n$ bye $"
-let strs = str.split('/')
-const validBlockTypes = ['create', 'loop']
+let str = "$[createvar] h]//i $/\n$[createvar] bye $"
+let strs = str.split('$/')
+
+strs.forEach((s, i) => strs[i] = i==strs.length-1?strs[i]:strs[i] + "$")
+
+const validBlockTypes = ['createvar','editval', 'root']
 let codeblocks = []
+
 for (let line in strs) {
     let blocktxt = strs[line].substring(
         strs[line].indexOf("$") + 1, 
         strs[line].lastIndexOf("$")
-    ).trim()
+    )
     
     let blocktype = blocktxt.substring(
-        blocktxt.indexOf("[t->") + 1,
+        blocktxt.indexOf("[") + 1,
         blocktxt.indexOf("]")
-    ).trim()
+    )
     
-    if (!validBlockTypes.includes(blocktype)) return //console.error
+    if (!validBlockTypes.includes(blocktype)) return console.error('noob')//console.error
+    let code = {
+      str: blocktxt.replace(/\[.*?\]/, '').trim(),
+      type: blocktype
+    }
+    codeblocks.push(code)
 }
 console.log(codeblocks)
