c7a57ce2f993e3e799eeaf99f190ff5d620ab28f
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2022-09-30 22:05:20 +0530

    functions done

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-let str = "$[createvar] name : flaming < \"\"$/\n$[createvar] age : 17 < 09$/\n$[root] => my name: >name<\nmy age: >age< $"
-let strs = str.split('$/')
+const fs = require('fs')
+let filepath = './index.txt'
+let codetxt = fs.readFileSync(filepath, 'utf-8')
+let strs = codetxt.split('$/')
 
-function breakCode (strs) {
-    strs.forEach((s, i) => strs[i] = i==strs.length-1?strs[i]:strs[i] + "$")
+function breakCode(strs) {
+    strs.forEach((s, i) => strs[i] = i == strs.length - 1 ? strs[i] : strs[i] + "$")
 
-    const validBlockTypes = ['createvar','editval','loop', 'root']
+    const validBlockTypes = ['createvar', 'editval', 'loop', 'root']
     let codeblocks = []
-
     for (let line in strs) {
         let blocktxt = strs[line].substring(
-            strs[line].indexOf("$") + 1, 
+            strs[line].indexOf("$") + 1,
             strs[line].lastIndexOf("$")
         )
-    
+
         let blocktype = blocktxt.substring(
             blocktxt.indexOf("[") + 1,
             blocktxt.indexOf("]")
         )
-    
-        if (!validBlockTypes.includes(blocktype)) return console.error('noob')//console.error
+
+        if (!validBlockTypes.includes(blocktype)) throw new Error('noob')//throw new Error
         let code = {
             str: blocktxt.replace(/\[.*?\]/, '').trim() + ';',
             type: blocktype
         }
         codeblocks.push(code)
     }
-return codeblocks
+    return codeblocks
 }
 
 //prolly will go in another file
@@ -34,102 +35,108 @@ memory.variables = {}
 
 interp(breakCode(strs))
 
-function interp (codeblocks) {
-    
-for (let block in codeblocks){
-    let blocktxt = codeblocks[block].str
-        
-    let datatypeSym = blocktxt.substring(
-        blocktxt.lastIndexOf("<") + 1,
-        blocktxt.lastIndexOf(";")
-    ).trim()
-    let decl = blocktxt.slice(0, blocktxt.lastIndexOf('<'))
-    
-    if (codeblocks[block].type == 'createvar') {
-        if (!decl.includes(':')) return console.error('syntax error')
-        let declPos = decl.lastIndexOf(':')
-        
-        let varName = decl.slice(0, declPos).trim()
-        let value = decl.slice(declPos+1).trim()
-        value = dataParse(value, datatypeSym)
-        
-        memory.variables[varName] = value
-    }
-    else if (codeblocks[block].type == 'editval') {
-        if (!decl.includes(';')) return console.error('syntax error')
-        let declPos = decl.lastIndexOf(';')
-        
-        let varName = decl.slice(0, declPos).trim()
-        let value = decl.slice(declPos+1).trim()
-        value = dataParse(value, datatypeSym)
-        
-        varPath = varName.split('->').map(e => { return e.trim() })
-        
-        pathIndex(memory.variables, varPath, value)
-    } else if (codeblocks[block].type == 'loop') {
-        let validLoopSym = ['#', '@']
-    } else if (codeblocks[block].type == 'root') {
-        if (blocktxt.startsWith('=>')) {
+function interp(codeblocks) {
+    for (let block in codeblocks) {
+        let blocktxt = codeblocks[block].str
+
+        let datatypeSym = blocktxt.substring(
+            blocktxt.lastIndexOf("<") + 1,
+            blocktxt.lastIndexOf(";")
+        ).trim()
+        let decl = blocktxt.slice(0, blocktxt.lastIndexOf('<'))
+
+        if (codeblocks[block].type == 'createvar') {
+            if (!decl.includes(':')) throw new Error('syntax error')
+            let declPos = decl.indexOf(':')
+
+            let varName = decl.slice(0, declPos).trim()
+            let value = decl.slice(declPos + 1).trim()
+            let [parseValue, type] = dataParse(value, datatypeSym)
+
+            memory.variables[varName] = { value: parseValue, type: type }
+        }
+        else if (codeblocks[block].type == 'editval') {
+            if (!decl.includes(';')) throw new Error('syntax error')
+            let declPos = decl.indexOf(';')
+
+            let varName = decl.slice(0, declPos).trim()
+            let value = decl.slice(declPos + 1).trim()
+            let [parseValue, type] = dataParse(value, datatypeSym)
+
+            varPath = varName.split('->').map(e => { return e.trim() })
+
+            pathIndex(memory.variables, varPath, [parseValue, type])
+        } else if (codeblocks[block].type == 'loop') {
+            let validLoopSym = ['#', '@']
+        } else if (codeblocks[block].type == 'root') {
             let rawVal = blocktxt.slice(2, -1).trim()
-            let logVal = dataParse(rawVal, `""`)
-            console.log(logVal)
+            
+            if (blocktxt.startsWith('->')) {
+                let [parse] = dataParse(rawVal, `""`)
+                console.log(parse)
+            } else if (blocktxt.startsWith('=>')) {
+                if (!(rawVal in memory.variables)) throw new Error(`${rawVal} is undefined`)
+                if (memory.variables[rawVal].type != 'function') throw new Error(`${rawVal} is not a function`)
+
+                interp(breakCode(memory.variables[rawVal].value.split('$\\')))
+            }
         }
     }
 }
-}
-console.log(memory)
+//console.log(memory)
 
-function dataParse (value, sym) {
-    const validDataTypesSym  = ['\"\"', '09', '01', '[]', '{}', '(=>)']
+function dataParse(value, sym) {
+    const validDataTypesSym = ['\"\"', '09', '01', '[]', '{}', '(=>)']
     const validDataTypesStr = ['string', 'number', 'boolean', 'array', 'object', 'function']
-    
+
     const validDataTypes = new Map()
-        validDataTypesSym.forEach((sym, i) => validDataTypes.set(sym, validDataTypesStr[i]))
-        
+    validDataTypesSym.forEach((sym, i) => validDataTypes.set(sym, validDataTypesStr[i]))
+
     let datatype = validDataTypes.get(sym)
-    if (!validDataTypesSym.includes(sym)) return console.error('invalid datatype')
-    
+    if (!validDataTypesSym.includes(sym)) throw new Error('invalid datatype')
+
     if (value.includes('>') && value.includes('<')) {
         let varName = value.substring(
             value.indexOf(">") + 1,
             value.indexOf("<")
         ).trim()
-        
-        if (memory.variables[varName] == undefined) return console.error(`${varName} is undefined`)
-        
-        let newVal = value.replace(/\>.*?\</, memory.variables[varName])
-        
+
+        if (memory.variables[varName] == undefined) throw new Error(`${varName} is undefined`)
+
+        let newVal = value.replace(/\>.*?\</, memory.variables[varName].value)
+
         return dataParse(newVal, sym)
     }
     let parse
     if (datatype == 'string') parse = value.toString()
     if (datatype == 'number') {
         parse = Number(value)
-    if (isNaN(parse)) return console.error('datatype error')
-        }
+        if (isNaN(parse)) throw new Error('datatype error')
+    }
     if (datatype == 'boolean') {
         if (value == '0' || value == 'false') parse = false
         if (value == '1' || value == 'true') parse = true
-        else console.error('datatype error')
-        }
-        if (datatype == 'array') parse = value.split(',')
-        if (datatype == 'object') {
-            let obj = {}
-            value.split(',').forEach(e => {
-                if(e.includes('-')) {
-                    let key = e.split('-')[0]
-                    let val = e.split('-')[1]
-                    obj[key] = val
+        else throw new Error('datatype error')
+    }
+    if (datatype == 'array') parse = value.split(',')
+    if (datatype == 'object') {
+        let obj = {}
+        value.split(',').forEach(e => {
+            if (e.includes('-')) {
+                let key = e.split('-')[0]
+                let val = e.split('-')[1]
+                obj[key] = val
             }
         })
         parse = obj
     }
-    return parse
+    if (datatype == 'function') parse = value
+    return [parse, datatype]
 }
 
-function pathIndex (obj,is, value) {
-    if (is.length==0) return
-    if (obj[is[0]] !== undefined) pathIndex(obj[is[0]],is.slice(1), value);
-    else return console.error(`${is[0]} is undefined property`)
-    if (is.length==1) return obj[is[0]] = value;
+function pathIndex(obj, is, value) {
+    if (is.length == 0) return
+    if (obj[is[0]] !== undefined) pathIndex(obj[is[0]], is.slice(1), value);
+    else throw new Error(`${is[0]} is undefined property`)
+    if (is.length == 1) return obj[is[0]] = { value: value[0], type: value[1] };
 }
