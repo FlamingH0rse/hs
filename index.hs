60427fc05429c182d4544d51f16901ef4623ec10
FlamingH0rse <99073459+FlamingH0rse@users.noreply.github.com>
2022-10-01 11:54:40 +0530

    i suck at hs

--- index.hs
+++ index.hs
@@ -2,6 +2,6 @@ $[createvar] object : name-flame,age-17 < {}$/
 $[createvar] name : Flaming < ""$/
 $[createvar] myfunc : 
     $[root] -> hello world$\ 
-    $[editval] -> name ; Chronny$\    
+    $[editval] -> name ; Chronny < ""$\    
 < (=>) $/
 $[root] => myfunc$/
