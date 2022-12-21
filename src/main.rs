d86559d44dc0217089436af8ecfa4ad5712b9a88
flightofnoobs <42741758+flightofnoobs@users.noreply.github.com>
2022-12-21 19:48:37 +0800

    EZ

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use std::env;
 
 fn main() {
-    let cmd_args: Vec<String> = env::args().collect();
-    println!("{:?}", cmd_args);
+    let mut cmd_args= env::args();
+
+    cmd_args.next();
+
+    if let Some(args) = cmd_args.next() {
+        println!("sup");
+        println!("{args}");
+    } else {
+        println!("Welcome to H0rseScript v1.0.");
+        println!("Type \"hs <file_name>\" to run it.");
+
+        loop {
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).expect("nerd u suck");
+        }
+    }
 }
+
