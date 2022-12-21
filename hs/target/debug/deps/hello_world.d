613c880f9a562ab42294170072ad094c9af1ec47
flightofnoobs <42741758+flightofnoobs@users.noreply.github.com>
2022-12-21 18:40:19 +0800

    loser got rekt btw

--- hs/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-D:\Projects\hs\rusttest\hello_world\target\debug\deps\hello_world.exe: src\main.rs
-
-D:\Projects\hs\rusttest\hello_world\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:
--- hs/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-D:\Projects\hs\rusttest\hello_world\target\debug\hello_world.exe: D:\Projects\hs\rusttest\hello_world\src\main.rs
