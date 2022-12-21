a2887df74721e0b9c49a35fe1a37d6e1786a902b
flightofnoobs <42741758+flightofnoobs@users.noreply.github.com>
2022-12-21 22:22:27 +0800

    fixed design not showing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, io::Write};
 
 fn main() {
     let mut cmd_args= env::args();
@@ -12,10 +12,11 @@ fn main() {
         // If there are no arguments specified open the program instead (Similar to Python ande Node.js)
         println!("Welcome to H0rseScript v1.0.");
         println!("Type \"hs <file_name>\" to run it.");
-
+       
         loop {
-            let mut input = String::new();
             print!(">> "); // fancy design
+            std::io::stdout().flush().unwrap(); // Flush the buffer (idk what exactly is this for)
+            let mut input = String::new();
             std::io::stdin().read_line(&mut input).expect("nerd u suck");
         }
     }
