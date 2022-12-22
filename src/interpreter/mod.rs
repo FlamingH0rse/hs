b556adb30a7a6264f37f0aa75e9b84ff9c109915
flightofnoobs <42741758+flightofnoobs@users.noreply.github.com>
2022-12-22 21:48:14 +0800

    ok

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -1,6 +1,7 @@
 use std::process::Command;
 
 pub fn run_nodejs(_file: String) {
+    // You can add more .arg() to pass in args
     Command::new("node")
         .arg("./nodejs/index.js")
         .spawn()
