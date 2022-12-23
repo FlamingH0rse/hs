ee9f5d1092d325d21be6ec6d1fce14bc6a0fe223
flightofnoobs <42741758+flightofnoobs@users.noreply.github.com>
2022-12-23 21:58:08 +0800

    im too stupid for this nvm

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,5 +1,10 @@
 // Help command
+use super::interpreter;
 
 pub fn help(){
     println!("PLS WORK BRUH PLS WORK AAAAAAA");
+}
+
+pub fn run(args: std::path::PathBuf) {
+    interpreter::run_nodejs(args);
 }
